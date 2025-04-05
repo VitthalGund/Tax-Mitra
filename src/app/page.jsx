a7244ee4e54ae0cbd080764bdac74da2e5e12e92
ITS-DNY <dnyandeepgaonkar23@gmail.com>
2025-04-05 18:27:17 +0530

    Animations Done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,13 @@ import Fetures from "@/components/Home/Fetures";
 import Testimonial from "@/components/Home/Testimonial";
 import UserSpecificBenifit from "@/components/Home/UserSpecificBenifit";
 import Cta from "@/components/Home/CTA";
+import ChatBot from "./bot";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col ">
       <main className="flex-1 p-20 md:p-32 bg-gradient-to-b  from-white to-teal-50">
+        <ChatBot/>
         {/* Hero Section */}
        <Herosection/>
         {/* Features Section */}
