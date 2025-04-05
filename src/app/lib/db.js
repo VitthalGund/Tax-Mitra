9ce38bc3c978ac03786b90a8e877a1573a16716e
Vitthal Popat Gund <97181033+VitthalGund@users.noreply.github.com>
2025-04-06 04:19:06 +0530

    db config .env changed

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGO_URL || "";
 
 if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable");
+    throw new Error("Please define the MONGO_URL environment variable");
 }
 
 let cached = global.mongoose || { conn: null, promise: null };
